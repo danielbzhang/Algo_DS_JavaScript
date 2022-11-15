c0ca4505b3c762a76ae9fc18ef5157d6c267cb41
Daniel Zhang <daniel6628@gmail.com>
2022-11-15 10:41:07 -0500

    reversestring done

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,45 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   if (str.length > 0) {
+//     const newStr = str.split('').reverse().join('');
+//     return newStr;
+//   } else {
+//     return;
+//   }
+// }
+
+// function reverse(str) {
+//   const myArr = [];
+//   const arr = str.split('');
+
+//   for (let index = arr.length - 1; index >= 0; index--) {
+//     const element = arr[index];
+//     myArr.push(element);
+//   }
+//   const result = myArr.join('');
+//   return result;
+// }
+
+// function reverse(str) {
+//   let reversed = '';
+//   for (const letter of str) {
+//     reversed = letter + reversed;
+//   }
+//   return reversed;
+// }
+
+function reverse(str) {
+  let arr = str.split('');
+  const result = arr.reduce((acc, cur) => {
+    return cur + acc;
+  }, '');
+
+  return result;
+}
+
+console.log(reverse('apple'));
+// reverse('apple');
 
 module.exports = reverse;
