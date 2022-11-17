4219162660e0ba5908b008c2c64e50e6e19f41e0
Daniel Zhang <daniel6628@gmail.com>
2022-11-17 10:00:49 -0500

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,32 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const obj = {};
+  // for (let i = 0; i < str.length; i++) {
+  //   const element = str[i];
+  //   if (!obj[element]) {
+  //     obj[element] = 1;
+  //   } else {
+  //     obj[element]++;
+  //   }
+  // }
+  for (let char of str) {
+    obj[char] = obj[char] + 1 || 1;
+  }
+
+  let max = 0;
+  let maxChar = '';
+  for (key in obj) {
+    if (obj[key] > max) {
+      max = obj[key];
+      maxChar = key;
+    }
+  }
+  return maxChar;
+  // console.log(max);
+}
+
+console.log(maxChar('hhhellllooooo'));
 
 module.exports = maxChar;
