57942b723a5473386e5616df9fa632146fb1ae57
Daniel Zhang <daniel6628@gmail.com>
2022-11-19 11:01:48 -0500

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,50 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// helper function
+function buildCharMap(str) {
+  str = str.replace(/[^\w]/g, '').toUpperCase();
+  let obj = {};
+
+  for (let ele of str) {
+    !obj[ele] ? (obj[ele] = 1) : obj[ele]++;
+  }
+  // console.log(obj);
+
+  return obj;
+}
+
+function anagrams(stringA, stringB) {
+  const objA = buildCharMap(stringA);
+  const objB = buildCharMap(stringB);
+
+  if (Object.keys(objA).length === Object.keys(objB).length) {
+    for (let keyA in objA) {
+      if (objA[keyA] !== objB[keyA]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// another way:
+function helperFunc(str) {
+  return str.replace(/[^\w]/g, '').toUpperCase().split('').sort().join('');
+}
+
+function anagrams1(stringA, stringB) {
+  return helperFunc(stringA) === helperFunc(stringB);
+}
+
+console.log(anagrams('rail safety', 'fairy tales'));
+console.log(anagrams('RAIL! SAFETY!', 'fairy tales'));
+console.log(anagrams('Hi there', 'Bye there'));
+
+console.log(anagrams1('rail safety', 'fairy tales'));
+console.log(anagrams1('RAIL! SAFETY!', 'fairy tales'));
+console.log(anagrams1('Hi there', 'Bye there'));
 
 module.exports = anagrams;
