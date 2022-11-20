de3fc71bf8a563a1cb13cefecf7265a356d7126d
Daniel Zhang <daniel6628@gmail.com>
2022-11-20 10:01:35 -0500

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,21 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let arr = str.split(' ');
+  const array = [];
+
+  for (let w of arr) {
+    // w = w.charAt(0).toUpperCase() + w.slice(1, w.length);
+    w = w[0].toUpperCase() + w.slice(1);
+    array.push(w);
+  }
+  const result = array.join(' ');
+  return result;
+}
+
+console.log(capitalize('a short sentence'));
+console.log(capitalize('a lazy fox'));
+console.log(capitalize('look, it is working!'));
 
 module.exports = capitalize;
