8cb681d124a83b4e59a830b8c41c693c1ff477cb
Daniel Zhang <daniel6628@gmail.com>
2022-12-05 10:48:37 -0500

    fib_memoi

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,7 +8,21 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
   if (n === 1) {
     return 0;
   }
@@ -22,9 +36,10 @@ function fib(n) {
   }
 }
 
+const fib = memoize(slowFib);
+
 console.log(fib(3));
-console.log(fib(5));
 console.log(fib(10));
-console.log(fib(20));
+console.log(fib(30));
 
 module.exports = fib;
